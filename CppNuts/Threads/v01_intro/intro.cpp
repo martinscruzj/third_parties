7f52d028078af6463b4878c8651622158bf65bc4
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-02 11:55:07 -0700

    First approach with no threads

--- CppNuts/Threads/v01_intro/intro.cpp
+++ CppNuts/Threads/v01_intro/intro.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <chrono>
+
+typedef unsigned long long ull;
+
+ull EvenSum = 0;
+ull OddSum = 0;
+
+void findEven(ull start, ull end) {
+   for (ull i = start; i <= end; ++i) {
+      if ((i & 1) == 0) {
+         EvenSum += i;
+      }
+   }
+}
+
+void findOdd(ull start, ull end) {
+   for (ull i = 0; i <= end; ++i) {
+      if ((i & 1) == 1) {
+         OddSum += i;
+      }
+   }
+}
 
 int main(int argc, char** argv) {
-   std::cout << "Hello, World!" << std::endl;
+   ull start = 0, end = 1900000000;
+   
+   auto startTime = std::chrono::high_resolution_clock::now();
+
+   findOdd(start, end);
+   findEven(start, end);
+   
+   auto stopTime = std::chrono::high_resolution_clock::now();
+   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stopTime - startTime);
+
+   std::cout << "OddSum : " << OddSum << std::endl;
+   std::cout << "EvenSum : " << EvenSum << std::endl;
+   std::cout << "Sec: " << duration.count()/1000000 << std::endl;
+
    return 0;
 }
