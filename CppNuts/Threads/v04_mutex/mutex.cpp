5fdb52acab1f33a4d29ac3157c520c138b3e9180
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-03 15:10:32 -0700

    Add mutex protection

--- CppNuts/Threads/v04_mutex/mutex.cpp
+++ CppNuts/Threads/v04_mutex/mutex.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+int myAmount = 0;
+std::mutex m;
+
+void addMoney() {
+    m.lock();
+    ++myAmount;
+    m.unlock();
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
+    std::thread t1(addMoney);
+    std::thread t2(addMoney);
+
+    t1.join();
+    t2.join();
+
+    std::cout << myAmount << std::endl;
 }
