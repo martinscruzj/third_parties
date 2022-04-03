78685450eff9a072b948640787c30f25127ec3bf
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-02 20:47:43 -0700

    Create threads using lambda, functor, non-static method, and static method

--- CppNuts/Threads/v02_creation_types/creation_types.cpp
+++ CppNuts/Threads/v02_creation_types/creation_types.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
 #include <thread>
 
-void fun(int x) {
+void fun1(int x) {
     while (x-- > 0) {
         std::cout << x << std::endl;
     }
 }
 
+class Functor {
+public:
+    void operator()(int x) {
+        while (x-- > 0) {
+            std::cout << x << std::endl;
+        }
+    }
+};
+
+class MyClass {
+public:
+    void run1(int x) {
+        while (x-- > 0) {
+            std::cout << x << std::endl;
+        }
+    }
+    static void run2(int x) {
+        while (x-- > 0) {
+            std::cout << x << std::endl;
+        }
+    }
+};
+
 int main(int argc, char** argv) {
-    std::thread t(fun, 10);
-    t.join();
+    // Create thread using function pointer
+    std::thread t1(fun1, 10);
+
+    // Create thread using lambda function
+    std::thread t2([](int x) {
+        while (x-- > 0) {
+            std::cout << x << std::endl;
+        }
+    }, 10);
+    
+    // Create thread using functor
+    std::thread t3((Functor()), 10);
+    
+    // Create thread using non-static member function
+    MyClass b;
+    std::thread t4(&MyClass::run1, &b, 10);
+    
+    std::thread t5(&MyClass::run2, 10);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
     return 0;
 }
