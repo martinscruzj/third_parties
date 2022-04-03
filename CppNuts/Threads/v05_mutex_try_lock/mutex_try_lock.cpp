13e5e5a0f1f9b148034e3233d9dbc107bb78803c
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-03 15:49:39 -0700

    Add mutex try lock logic

--- CppNuts/Threads/v05_mutex_try_lock/mutex_try_lock.cpp
+++ CppNuts/Threads/v05_mutex_try_lock/mutex_try_lock.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+
+int counter = 0;
+std::mutex mtx;
+
+void increaseTheCounterFor100000Time() {
+    for (int i = 0; i < 100000; i++) {
+        if (mtx.try_lock()) {
+            ++counter;
+            mtx.unlock();
+        }
+    }
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, Wold!" << std::endl;
+    std::thread t1(increaseTheCounterFor100000Time);
+    std::thread t2(increaseTheCounterFor100000Time);
+
+    t1.join();
+    t2.join();
+
+    std::cout << " counter could increase upto: " << counter << std::endl;
+
+    return 0;
 }
