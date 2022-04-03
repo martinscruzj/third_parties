dc2e1c0c465db50fdc777d9f79848f8142821488
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-03 11:45:46 -0700

    Double detach example

--- CppNuts/Threads/v03_join_detach/join_detach.cpp
+++ CppNuts/Threads/v03_join_detach/join_detach.cpp
@@ -4,18 +4,21 @@
 void run(int count) {
     using namespace std::literals::chrono_literals;
     while (count-- > 0) {
-        std::cout << "CppNuts" << std::endl;
+        std::cout << count << " CppNuts" << std::endl;
     }
-    std::this_thread::sleep_for(5s);
+    //std::this_thread::sleep_for(5s);
+    std::cout << "thread finished" << std::endl;
 }
 
 int main(int argc, char** argv) {
+    using namespace std::literals::chrono_literals;
     std::thread t1(run, 10);
     std::cout << "main()" << std::endl;
-    t1.join();
+    t1.detach();
     if (t1.joinable()) {
-        t1.join();
+        t1.detach();
     }
     std::cout << "main() after" << std::endl;
+    //std::this_thread::sleep_for(5s);
     return 0;
 }
