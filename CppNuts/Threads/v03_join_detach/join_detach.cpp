56ac21dae1b4863fce69a503bbf63b32ab13c334
Martin <101901076+martinscruzj@users.noreply.github.com>
2022-04-03 11:31:20 -0700

    Double join example

--- CppNuts/Threads/v03_join_detach/join_detach.cpp
+++ CppNuts/Threads/v03_join_detach/join_detach.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <thread>
+
+void run(int count) {
+    using namespace std::literals::chrono_literals;
+    while (count-- > 0) {
+        std::cout << "CppNuts" << std::endl;
+    }
+    std::this_thread::sleep_for(5s);
+}
 
 int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
+    std::thread t1(run, 10);
+    std::cout << "main()" << std::endl;
+    t1.join();
+    if (t1.joinable()) {
+        t1.join();
+    }
+    std::cout << "main() after" << std::endl;
     return 0;
 }
